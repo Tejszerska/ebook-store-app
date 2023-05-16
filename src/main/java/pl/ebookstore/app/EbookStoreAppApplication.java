273cc20bda77a4375d7706a43a3ed758082ceb5f
Basia <barbarawojciechowskaa@gmail.com>
2023-05-16 20:44:57 +0200

    Initial commit

---
+++ src/main/java/pl/ebookstore/app/EbookStoreAppApplication.java
@@ -0,0 +1,13 @@
+package pl.ebookstore.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookStoreAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ebookstore/app/EbookStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ebookstore.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
